97e3ca97c6a783a75bebdc7b140fc5feb0a9bb06
Orinav <orinave10@gmail.com>
2026-05-13 15:56:53 +0300

    Updated the url in scraper.py and hide the API_KEY in .env file for better practice

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,5 +1,7 @@
 import requests
 import logging
+import os
+from dotenv import load_dotenv
 from database import db_reset, db_insert
 
 logger = logging.getLogger(__name__)
@@ -7,7 +9,8 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5amZmcm1maXJrd2N3ZW1wYXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3NzAyNzgsImV4cCI6MjA0MjM0NjI3OH0.clLm3KrW9nuWtWRgL4VXz2dH0zohot2Q3XqQ1lSRelI"
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+API_KEY = os.getenv('ELOSHAPES_API_KEY')
 
 
 def get_image_url(item):
@@ -30,7 +33,7 @@ def get_image_url(item):
     return ""
 
 def scrape_eloshapes_api():
-    url = "https://qyjffrmfirkwcwempawu.supabase.co/rest/v1/products_available_v8?select=general__handle%2Cgeneral__category%2Cgeneral__brand_names%2Cgeneral__brands_separator%2Cgeneral__model%2Cgeneral__variant%2Cgeneral__status_edited_date%2Cgeneral__affiliate_links%2Cgeneral__images%2Cmouse__size_rating%2Cmouse__size_category%2Cmouse__length%2Cmouse__width%2Cmouse__height%2Cmouse__weight%2Cmouse__shape%2Cmouse__hump_placement%2Cmouse__front_flare%2Cmouse__side_curvature%2Cmouse__hand_compatibility%2Cmouse__thumb_rest%2Cmouse__ring_finger_rest%2Cmouse__wireless%2Cmouse__dpi%2Cmouse__polling_rate%2Cmouse__side_buttons%2Cmouse__middle_buttons%2Cmouse__top_view%2Cmouse__side_view%2Cmouse__back_view%2Cmouse__material_name_general%2Cmouse__material_name_specific%2Cmouse__sensor_handle%2Cmouse__sensor_brand_names%2Cmouse__sensor_brands_separator%2Cmouse__sensor_model%2Cmouse__sensor_variant%2Cmouse__sensor_rank%2Cmouse__sensor_type%2Cmouse__sensor_tracking_speed%2Cmouse__sensor_acceleration%2Cmouse__adjustable_sensor_position%2Cmouse__sensor_position_x%2Cmouse__sensor_position_x2%2Cmouse__sensor_position_y%2Cmouse__sensor_position_y2%2Cmouse__hot_swappable_switches%2Cmouse__switch_objects%2Cmouse__scroll_wheel_encoder_objects&general__category=eq.mouse"
+    url = "https://qyjffrmfirkwcwempawu.supabase.co/rest/v1/products_available_v13_0bnvb?select=general__handle%2Cgeneral__category%2Cgeneral__brand_names%2Cgeneral__brands_separator%2Cgeneral__model%2Cgeneral__variant%2Cgeneral__status_edited_date%2Cgeneral__affiliate_links%2Cgeneral__images%2Cmouse__size_rating%2Cmouse__size_category%2Cmouse__length%2Cmouse__width%2Cmouse__height%2Cmouse__weight%2Cmouse__shape%2Cmouse__hump_placement%2Cmouse__front_flare%2Cmouse__side_curvature%2Cmouse__hand_compatibility%2Cmouse__thumb_rest%2Cmouse__ring_finger_rest%2Cmouse__wireless%2Cmouse__dpi%2Cmouse__polling_rate%2Cmouse__side_buttons%2Cmouse__middle_buttons%2Cmouse__top_view%2Cmouse__side_view%2Cmouse__back_view%2Cmouse__material_name_general%2Cmouse__material_name_specific%2Cmouse__sensor_handle%2Cmouse__sensor_brand_names%2Cmouse__sensor_brands_separator%2Cmouse__sensor_model%2Cmouse__sensor_variant%2Cmouse__sensor_rank%2Cmouse__sensor_type%2Cmouse__sensor_tracking_speed%2Cmouse__sensor_acceleration%2Cmouse__adjustable_sensor_position%2Cmouse__sensor_position_x%2Cmouse__sensor_position_x2%2Cmouse__sensor_position_y%2Cmouse__sensor_position_y2%2Cmouse__hot_swappable_switches%2Cmouse__switch_objects%2Cmouse__scroll_wheel_encoder_objects&general__category=eq.mouse"
     headers = {"apikey": API_KEY, "Authorization": f"Bearer {API_KEY}"}
 
     logger.info('Fetching ALL mice from EloShapes API...')
